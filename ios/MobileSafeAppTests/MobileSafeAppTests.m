41c2311679f3a2107b0e3a9e084885745dc87c2d
faiqueansari23 <ansarifaiqueakmal@gmail.com>
2024-10-24 18:52:59 +0530

    new changes

--- ios/MobileSafeAppTests/MobileSafeAppTests.m
+++ ios/MobileSafeAppTests/MobileSafeAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface anti_theft_userTests : XCTestCase
+@interface MobileSafeAppTests : XCTestCase
 
 @end
 
-@implementation anti_theft_userTests
+@implementation MobileSafeAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
